9b64159b90f51e209a2b478a419f260123436dc0
豈銓 張 <egg2915978@gmail.com>
2019-07-30 23:11:57 +0800

    2019/7/30 11:11

--- web/js/scroll.js
+++ web/js/scroll.js
@@ -1,11 +1,28 @@
 $(window).scroll(()=>{
     var top = $(window).scrollTop();
-    var w = $(window).width();
-    var first = $('#intro').height();
-    var value = top/(first-130);
-    $("#main-nav-container").css('background','rgba(245,245,245,'+value+')');
-    if(value<1)
+    var first = $('#intro').height()-100;
+    var value1 = top/first;
+
+    $("#main-nav-container").css('background','rgba(245,245,245,'+value1+')');
+
+    if(value1<1)
         $("#nav-intro").css('color','#FF5511');
     else
-    $("#nav-intro").css('color','black');
+        $("#nav-intro").css('color','black');
+
+    var second = first+$('#about').height();
+    if(top>first &&top<second)
+        $("#nav-about").css('color','#FF5511');
+    else
+        $("#nav-about").css('color','black');
+    var third = second+$('#works').height();
+    if(top>second &&top<third)
+        $('#nav-works').css('color','#FF5511');
+    else
+        $('#nav-works').css('color','black');
+    
+    if(top>third)
+        $('#nav-links').css('color','#FF5511');
+    else
+        $('#nav-links').css('color','black');
 })
