fcb71591b36ca34e654abffacc104fcfe0912880
豈銓 張 <egg2915978@gmail.com>
2019-07-30 17:42:05 +0800

    .

--- web/js/scroll.js
+++ web/js/scroll.js
@@ -4,6 +4,8 @@ $(window).scroll(()=>{
     var first = $('#intro').height();
     var value = top/(first-130);
     $("#main-nav-container").css('background','rgba(245,245,245,'+value+')');
-    if(w <=426)
-        $(".nav-ul li").css('background','rgba(245,245,245,'+value+')');
+    if(value<1)
+        $("#nav-intro").css('color','#FF5511');
+    else
+    $("#nav-intro").css('color','black');
 })
